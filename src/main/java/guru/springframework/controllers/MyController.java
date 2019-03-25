5c055265e31d9528b82a946d088469e0d3599297
Marcin <mwro91@gmail.com>
2019-03-25 22:07:09 +0100

    context test

--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -1,4 +1,10 @@
 package guru.springframework.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MyController {
+    public void hello() {
+        System.out.println("hello!");
+    }
 }
